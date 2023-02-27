249512717a9900486a1599d817f49df5a21d0fff
edfranco <edfranco415@gmail.com>
2023-02-26 23:56:28 -0800

    created landing

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Food Envy</Text>
-      <StatusBar style="auto" />
+      <View style={styles.logoContainer}>
+        <Text style={{color:'#fff', fontSize:36}}>FoodEnvy</Text>
+      </View>
+      <View style={styles.authContainer}>
+        <Pressable style={[styles.authButtons, {marginVertical:3}]}>
+          <Text style={[styles.authButtonsText]}>Log In</Text>
+        </Pressable>
+        <Pressable style={[styles.authButtons]}>
+          <Text style={styles.authButtonsText}>Sign Up</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logoContainer: {
+    flex:5,
+    justifyContent:'center'
+  },
+  authContainer: {
+    flex: 1,
+    width:'100%'
+  },
+  authButtons: {
+    textAlign:'center',
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#b30000',
+    height:50
+  },
+  authButtonsText: {
+    fontSize:24,
+    color:'#fff'
+  }
 });
